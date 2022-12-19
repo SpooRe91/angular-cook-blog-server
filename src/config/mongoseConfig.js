66e300bd1ce67f0240635f6a5a00234963b57edc
Martin B <m.bogdanov9110@gmail.com>
2022-12-19 23:40:10 +0200

    put the string

--- src/config/mongoseConfig.js
+++ src/config/mongoseConfig.js
@@ -5,7 +5,7 @@ exports.connecter = async () => {
     try {
         mongoose.connection.on('open', () => console.log('DB connected'));
         mongoose.set("strictQuery", false);
-        return mongoose.connect(process.env.MONGO_STRING);
+        return mongoose.connect("mongodb+srv://SpooRe91:yUXNuwLoeOGSApso@cook-blog.qcll9bj.mongodb.net/test");
     } catch (error) {
         console.log('Error connecting to database');
         return process.exit(1);
