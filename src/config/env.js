0abee8fecfe1fd6418274a6bdafe7397b2181001
Martin B <m.bogdanov9110@gmail.com>
2022-12-19 22:11:54 +0200

    update

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,3 @@
 exports.secret = 'asdawdwwdadajdawjdoiajd';
 exports.PORT = 3030;
-exports.DB_QUERY_STRING = 'mongodb://0.0.0.0:27017/cooking';
+exports.DB_QUERY_STRING = `mongodb+srv://SpooRe91:yUXNuwLoeOGSApso@cook-blog.qcll9bj.mongodb.net/test`;
