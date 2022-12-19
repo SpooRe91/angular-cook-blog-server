0123c681c5e3c3ff7a84421ed170b86d2cb63c15
Martin B <m.bogdanov9110@gmail.com>
2022-12-19 22:37:30 +0200

    update

--- src/config/mongoseConfig.js
+++ src/config/mongoseConfig.js
@@ -4,9 +4,8 @@ const { DB_QUERY_STRING } = require('./env');
 
 exports.connecter = async () => {
     try {
-        console.log(DB_QUERY_STRING);
-        mongoose.set("strictQuery", false);
         mongoose.connection.on('open', () => console.log('DB connected'));
+        mongoose.set("strictQuery", false);
         return mongoose.connect(DB_QUERY_STRING);
     } catch (error) {
         console.log('Error connecting to database');
