c85524e665e3bc94ae4df32f6f19884943a87e8e
Martin B <m.bogdanov9110@gmail.com>
2023-01-12 22:36:59 +0200

    update, cors, removed cors middleware

--- src/config/mongoseConfig.js
+++ src/config/mongoseConfig.js
@@ -5,7 +5,7 @@ exports.connecter = async () => {
     try {
         mongoose.connection.on('open', () => console.log('DB connected'));
         mongoose.set("strictQuery", false);
-        return mongoose.connect("mongodb+srv://SpooRe91:yUXNuwLoeOGSApso@cook-blog.qcll9bj.mongodb.net/test");
+        return mongoose.connect(process.env.MONGO_STRING);
     } catch (error) {
         console.log('Error connecting to database');
         return process.exit(1);
--- src/index.js
+++ src/index.js
@@ -3,22 +3,20 @@ const express = require('express');
 const dbService = require('./config/mongoseConfig');
 const { auth } = require('./middlewares/authMiddleware');
 const { errorHandler } = require('./middlewares/errorHandlerMiddleware');
-const cors = require('./middlewares/corsMiddleware');
+const cors = require('cors');
 const app = express();
-
-const currPort = 3030;
 
 dbService.connecter();
 require('./config/cookieParserConfig')(app);//cookie parser
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:4200', "https://angular-cook-blog.web.app", 'http://localhost:3000'], credentials: true }));
 app.use(auth);//auth middleware
 require('./config/expressConfig')(app);//express config
 app.use(errorHandler)//error handler
 
-app.listen(currPort, (error) => {
+app.listen(process.env.PORT, (error) => {
   if (error) {
     console.log(error.message);
   }
-  console.log(`"Cook-blog API" listening to port http://localhost:${3030}`);
+  console.log(`"Cook-blog API" listening to port http://localhost:${process.env.PORT}`);
   console.log(`"Cook-blog" Angular APP and REST API documentation is available at: https://github.com/SpooRe91/angular-cook-blog/blob/main/README.md`);
 });
